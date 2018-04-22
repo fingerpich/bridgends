1cb31f8b4bb2f933e199f56894f12e20507e4399
mojtaba <zarei.bs@gmail.com>
2018-04-22 18:15:03 +0430

    add a module to cache requests in files

---
+++ src/cacheResponds/cache.js
@@ -0,0 +1,68 @@
+const filendir = require('filendir');
+const crypto = require('crypto');
+const Envelope = require('./envelope.js');
+const hasher = crypto.createHash('sha256');
+
+class CacheResponds {
+    constructor () {
+
+    }
+
+    start () {
+
+    }
+
+    saveRequest (envelope) {
+        const filePath = this._getFileName(envelope);
+        filendir.writeFile(filePath, JSON.stringify(envelope), function (err) {
+            if (err) {
+                log('File could not be saved in ' + this.config.cacheDir);
+                throw err
+            }
+        }.bind(this));
+    }
+
+    respond (req) {
+        this.getRequest(req).getData();
+    }
+
+    has (req) {
+        const filePath = this._getFileName(req);
+        return fs.existsSync(filePath);
+    }
+
+    getRequest (req) {
+        return new Promise((resolve, reject) => {
+            const filePath = this._getFileName(req);
+            try {
+                fs.readFile(filePath, 'utf-8', (err, data) => {
+                    if (err) reject('')
+                });
+                const cachedEnvelope = Envelope.fromJson(data);
+                if (cachedEnvelope.checkVersion()) {
+                    reject('Request envelope created in old plugin version.');
+                } else {
+                    resolve(cachedEnvelope);
+                }
+            } catch(e) {
+                reject(e);
+            }
+        });
+    }
+
+    _getFileName (envelope) {
+        let bodyHash = '';
+        if (envelope.reqMethod !== 'GET') {
+            hasher.update(envelope.reqBody);
+            bodyHash = hasher.digest('hex');
+        }
+
+        const sanitazedURL = sanitize(envelope.reqURL.replace('://', '-'), {replacement: '-'});
+        let fileName = envelope.reqMethod + '_' + sanitazedURL + bodyHash;
+        fileName = (fileName.length > 250) ? fileName.slice(0, 250) : fileName;
+        fileName += '.tmp';
+
+        return path.resolve(this.config.cacheDir, fileName);
+    }
+}
+module.exports = new CacheResponds();
