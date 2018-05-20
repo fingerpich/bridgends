697ff1ec6d6ed68bef10dfb4b71e1082b0d59fd2
mojtaba <zarei.bs@gmail.com>
2018-05-20 18:18:29 +0430

    update cli to start and stop some instance of bridgends

--- cli.js
+++ cli.js
@@ -6,63 +6,69 @@ const inquirer = require('inquirer');
 const config = require('./defaultconfig.js');
 
 program
-    .version('0.0.1', '-v, --version')
-    .option('-s, stop', 'stop')
-    .option('-t, --targets <targets>', 'Add target', list => list.split(','))
-    .option('-u, --ui-path', 'Set ui path')
-    .option('-f, --files-path', 'Set files path')
-    .option('-o, --socket-path', 'Set socket path')
-    .parse(process.argv);
+    .version('0.0.1', '-v, --version');
 
-if (program.stop) {
-    pm2.stop('index');
-    setTimeout(() => {
-        console.log('bridgends is stoped');
-        process.exit(2);
-    }, 2000);
-} else {
-    (() => {
-        if (program.targets) {
-            config.targets = program.targets;
-            console.log('targets: ' + config.targets);
-            return Promise.resolve()
-        } else {
-            return inquirer.prompt({type: 'string', name: 'targets', message: 'you have to had an api target at least, enter targets(eg: http://192.168.10.20:4243/)'})
-                .then(answers => {
-                    console.log(answers);
-                    config.targets = answers.targets.split(',');
-                    return config.targets;
-                },(err) => {
-                    console.error(err);
-                });
-        }
-    })().then(() => {
-        if (program.uiPath) {
-            config.uiPath = program.uiPath.split(':')[0];
-            config.port = program.uiPath.split(':')[1];
-        }
-        if (program.cachePath) {
-            config.saveDir = program.savePath;
-        }
-        if (program.socketPath) {
-            config.socketPath = program.socketPath.split(':')[0];
+program
+    .command('stop [name]')
+    .description('stop bridgends')
+    .action((name, cmd) => {
+        pm2.stop(name || 'index');
+        setTimeout(() => {
+            console.log('bridgends is stoped');
+            process.exit(2);
+        }, 2000);
+    });
 
-        }
-        fs.writeFileSync('./config.js',"module.exports ="+JSON.stringify(config));
-
-        pm2.connect(function(err) {
-            if (err) {
-                console.error(err.message);
-                process.exit(2);
+program
+    .command('start [name]')
+    .description('start new instance of bridgends')
+    .option('-t, --targets <targets>', 'set api targets', list => list.split(','))
+    .option('-u, --ui-path [value]', 'set instance ui path')
+    .option('-f, --save-path [value]', 'Set files path')
+    .option('-a, --api-path [value]', 'Set api path')
+    .option('-p, --port [value]', 'set instance port')
+    .option('-o, --socket-path [value]', 'Set socket path')
+    .action((name, cmd) => {
+        (() => {
+            if (cmd.targets) {
+                config.targets = cmd.targets;
+                console.log('targets: ' + config.targets);
+                return Promise.resolve()
+            } else {
+                return inquirer.prompt({type: 'string', name: 'targets', message: 'you have to had an api target at least, enter targets(eg: http://192.168.10.20:4243/)'})
+                    .then(answers => {
+                        config.targets = answers.targets.split(',');
+                        return config.targets;
+                    },(err) => {
+                        console.error(err);
+                    });
             }
-            console.log('bridgends has started');
-            console.log('open http://localhost' +(config.port?':'+config.port:'') + '/ in your browser');
-            pm2.start('index.js', function (err, apps) {
-                pm2.disconnect();
+        })().then(() => {
+            if (cmd.uiPath) {config.uiPath = cmd.uiPath;}
+            if (cmd.savePath) {config.saveDir = cmd.savePath;}
+            if (cmd.socketPath) {config.socketPath = cmd.socketPath;}
+            if (cmd.port) {config.port = cmd.port;}
+            fs.writeFileSync('./config.js', 'module.exports =' + JSON.stringify(config));
+
+            pm2.connect(function(err) {
                 if (err) {
                     console.error(err.message);
+                    process.exit(2);
                 }
+                console.log('bridgends has started');
+                console.log('open http://localhost' +(config.port?':'+config.port:'') + '/ in your browser');
+                pm2.start({name, script: 'index.js'}, function (err, apps) {
+                    pm2.disconnect();
+                    if (err) {
+                        console.error(err.message);
+                    }
+                });
             });
         });
     });
-}
+
+program
+    .parse(process.argv);
+
+
+if (program.args.length === 0) program.help();
